e162edbd3ed94f210ae282c03507df7cd9330fa9
Ishan Pranav <ishan.pranav@outlook.com>
2024-02-02 20:33:41 -0500

    Implement getEvenParam(...args)

--- src/hoffy.mjs
+++ src/hoffy.mjs
@@ -1,1 +1,27 @@
-// hoffy.mjs
+// Licensed under the MIT License.
+
+/**
+ * Filters the given arguments, returning a collection containig every other
+ * argument beginning with the first.
+ * 
+ * @param  {...any} args any number of string arguments.
+ * @return {Array} An array of every other argument beginning with the first, or
+ *                 an empty array if no arguments are supplied.
+ */
+export function getEvenParam(...args) {
+    let i = 0;
+    let j = 0;
+    let result = new Array(Math.trunc(args.length / 2));
+    
+    while (i < args.length) {
+        result[j] = args[i];
+        i += 2;
+        j++;
+    }
+
+    return result;
+}
+
+export function filterWith() {
+
+}
